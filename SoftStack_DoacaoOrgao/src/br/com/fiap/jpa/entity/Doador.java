9fdf1828841bf5c28e3fc4b87e9ac5f8eb946e50
dnlPolesi <49909534+DanielPolesi@users.noreply.github.com>
2020-04-25 01:18:22 -0300

    Add files via upload

---
+++ SoftStack_DoacaoOrgao/src/br/com/fiap/jpa/entity/Doador.java
@@ -0,0 +1,63 @@
+package br.com.fiap.jpa.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TB_DOADOR")
+@SequenceGenerator(name="doador", sequenceName = "SQ_TB_DOADOR", allocationSize = 1)
+public class Doador {
+
+	@Id
+	@Column(name="cd_doador")
+	@GeneratedValue(generator="doador", strategy = GenerationType.SEQUENCE)
+	private int codigoDoador;
+
+	@Column(name="nm_doador", nullable = false, length = 100)
+	private String nome;
+
+	@Column(name="cd_orgao_doador")
+	private int codigoOrgaoDoador;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name="ds_orgao_doador")
+	private TipoOrgao orgDoador; //descricao_orgao_doador Enum
+
+
+
+
+	public int getCodigoDoador() {
+		return codigoDoador;
+	}
+	public void setCodigoDoador(int codigoDoador) {
+		this.codigoDoador = codigoDoador;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getCodigoOrgaoDoador() {
+		return codigoOrgaoDoador;
+	}
+	public void setCodigoOrgaoDoador(int codigoOrgaoDoador) {
+		this.codigoOrgaoDoador = codigoOrgaoDoador;
+	}
+	public TipoOrgao getOrgDoador() {
+		return orgDoador;
+	}
+	public void setOrgDoador(TipoOrgao orgDoador) {
+		this.orgDoador = orgDoador;
+	}
+
+
+
+}
